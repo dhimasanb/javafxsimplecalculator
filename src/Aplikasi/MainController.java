c7fca892f62915e223777d00aa5408c37a539486
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2016-12-30 19:05:40 +0700

    final logika kalkulator

--- src/Aplikasi/MainController.java
+++ src/Aplikasi/MainController.java
@@ -8,7 +8,10 @@ package Aplikasi;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 /**
  * FXML Controller class
@@ -16,21 +19,52 @@ import javafx.fxml.Initializable;
  * @author Griya Salbini
  */
 public class MainController implements Initializable {
+    
+    @FXML
+    private Label result;
+    private long number1 = 0;
+    private String operator = "";
+    private boolean start = true;
+    private Model model = new Model();
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        // TODO 
     }
 
+    @FXML
     public void processNumbers(ActionEvent event){
-        
+        if (start) {
+            result.setText("");
+            start = false;
+        }
+        String value = ((Button)event.getSource()).getText();
+        result.setText(result.getText()+ value); 
     }
     
+    @FXML   
     public void processOperators(ActionEvent event){
+        String value = ((Button)event.getSource()).getText();
         
+        if (!value.equals("=")) {
+            if (!operator.isEmpty())
+                return;
+            
+            operator = value;
+            number1 = Long.parseLong(result.getText());
+            result.setText("");
+        } else {
+            if (operator.isEmpty())
+                return;
+            long number2 = Long.parseLong(result.getText());
+            float output = model.calculate(number1, number2, operator);
+            result.setText(String.valueOf(output));
+            operator = "";
+            start = true;
+        }
     }
     
 }
