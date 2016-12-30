1930f3d4e8e45da5f7787882869d11ecb8fd76b7
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2016-12-30 18:01:37 +0700

    tambah baru file modal

---
+++ src/Aplikasi/Modal.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aplikasi;
+
+/**
+ *
+ * @author Griya Salbini
+ */
+public class Modal {
+    public float calculate (long number1, long number2, String operator){
+        
+        return 0;
+    }
+}
